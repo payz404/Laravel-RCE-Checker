7e75b15d1b94e226775fd799904f2902fe5e7260
payz404 <46267341+payz404@users.noreply.github.com>
2020-12-13 23:27:07 +0700

    Add files via upload

---
+++ laravelrce/banner/__init__.py
@@ -0,0 +1,22 @@
+from laravelrce.packages import *
+
+class Banners:
+
+   def __init__(self):
+
+      banner = """      
+[red]      ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄
+      █▒▒░░░░░░░░░▒▒█
+       █░░█░░░░░█░░█
+    ▄▄  █░░░▀█▀░░░█  ▄▄
+   █░░█ ▀▄░░░░░░░▄▀ █░░█
+   █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+   █░░[green]╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗[/green]░░█ [/red]
+   █░░[green]║║║╠─║─║─║║║║║╠─[/green]░░█ 
+   █░░[green]╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝[/green]░░█ [i cyan]Made with a cup of[/cyan i] ☕
+   █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ [i cyan]Code by @im.payz[/cyan i]
+   
+    [bold magenta] Laravel RCE Checker[/magenta bold]
+   
+"""
+      print(banner)
